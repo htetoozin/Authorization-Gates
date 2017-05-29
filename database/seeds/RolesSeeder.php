d042cc49acf8c5d1896eff27ff68bd078a0f99d2
htetoozin <htetoo.zi09@gmail.com>
2017-05-29 18:15:13 +0630

    Laravel Authorization Gates

---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+
+class RolesSeeder extends Seeder
+{
+    public function run()
+    {
+        $author = Role::create([
+            'name' => 'Author', 
+            'slug' => 'author',
+            'permissions' => [
+                'create-post' => true,
+            ]
+        ]);
+        $editor = Role::create([
+            'name' => 'Editor', 
+            'slug' => 'editor',
+            'permissions' => [
+                'update-post' => true,
+                'publish-post' => true,
+            ]
+        ]);
+    }
+}
